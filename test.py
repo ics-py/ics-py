31e8a66918ccbe0747aac465bed03513c9c86869
iTitou <titouanchristophe@gmail.com>
2013-10-28 00:13:24 +0100

    [enh] Mo4r testz

--- test.py
+++ test.py
@@ -3,8 +3,10 @@ from parser import ParseError, ContentLine
 
 class TestContentLine(unittest.TestCase):
 	dataset = {
+		'haha:': ContentLine('haha'),
+		':hoho': ContentLine('',  {}, 'hoho'),
 		'haha:hoho': ContentLine('haha', {}, 'hoho'),
-		'haha:hoho:hoho': ContentLine('haha', {}, 'hoho:hoho'),
+		'haha:hoho:hihi': ContentLine('haha', {}, 'hoho:hihi'),
 		'haha;hoho=1:hoho': ContentLine('haha', {'hoho': ['1']}, 'hoho'),
 		'haha;p2=v2;p1=v1:': ContentLine('haha', {'p1':['v1'], 'p2':['v2']}, ''),
 		'haha;hihi=p3,p4,p5;hoho=p1,p2:blabla:blublu':ContentLine('haha', {'hoho':['p1','p2'], 'hihi':['p3','p4','p5']}, 'blabla:blublu'),
@@ -21,13 +23,13 @@ class TestContentLine(unittest.TestCase):
 		for test in self.dataset:
 			expected = self.dataset[test]
 			got = ContentLine.parse(test)
-			self.assertEqual(expected, got)
+			self.assertEqual(expected, got, "Parse")
 	
 	def test_str(self):
 		for test in self.dataset:
 			expected = test
 			got = str(self.dataset[test])
-			self.assertEqual(expected, got)
+			self.assertEqual(expected, got, "To string")
 
 if __name__ == '__main__':
 	unittest.main()
