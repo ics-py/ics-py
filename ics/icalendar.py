8f4b63313b45b53feb3624753859c51b522ade84
Nikita Marchant <nikita.marchant@gmail.com>
2013-10-29 17:34:24 +0100

    Parse METHOD, VEVENT, CALSCALE, VERSION and PRODID in icalendar.Calendar

--- ics/icalendar.py
+++ ics/icalendar.py
@@ -23,7 +23,51 @@ class Calendar(object):
         return k
 
     def populate(self, container):
+        #PRODID
         creators = filter(lambda x: x.name == 'PRODID', container)
         if len(creators) != 1:
             raise parse.ParseError('A calendar must have one and only one PRODID')
-        self.creator = creators[0].value
+        prodid = creators[0]
+        self.creator = prodid.value
+        self.creator_params = prodid.params
+
+        #VERSION
+        version = filter(lambda x: x.name == 'VERSION', container)
+        if len(version) != 1:
+            raise parse.ParseError('A calendar must have one and only one VERSION')
+        version = version[0]
+        self.creator_params = prodid.params
+        #TODO : should take care of minver/maxver
+        if ';' in version.value:
+            _, self.version = version.value.split(';')
+        else:
+            self.version = version.value
+        
+
+        #CALSCALE
+        calscale = filter(lambda x: x.name == 'CALSCALE', container)
+        if len(calscale) > 1:
+            raise parse.ParseError('A calendar must have at most one CALSCALE')
+
+        self.scale = 'georgian'
+        self.scale_params = {}
+        if len(calscale) == 1:
+            self.scale = calscale[0].value
+            self.scale_params[0].update(calscale.params)
+
+        #METHOD
+        method = filter(lambda x: x.name == 'METHOD', container)
+        if len(method) > 1:
+            raise parse.ParseError('A calendar must have at most one METHOD')
+
+        self.method = None
+        self.method_params = {}
+        if len(method) == 1:
+            self.method = method[0].value
+            self.method_params[0].update(method.params)
+
+        #VEVENT
+        events = filter(lambda x: x.name == 'VEVENT', container)
+        self.events = map(lambda x: Event.from_container(x),events)
+
+
