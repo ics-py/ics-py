11d70337ca826758f42d17e40b84561cf38100d5
Astalaseven <gatien.bovyn@gmail.com>
2014-01-01 23:21:38 +0100

    [pep8] Moar compliant

--- ics/__init__.py
+++ ics/__init__.py
@@ -5,4 +5,10 @@ from __future__ import unicode_literals, absolute_import
 
 from .icalendar import Calendar
 from .event import Event
-from .__meta__ import __title__, __version__, __author__, __license__, __copyright__
+from .__meta__ import (
+    __title__,
+    __version__,
+    __author__,
+    __license__,
+    __copyright__,
+)
