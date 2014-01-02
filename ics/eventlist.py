ff4aba96fa39a5347b8d009a9b2d7f8cb5b5eaf1
iTitou <titouanchristophe@gmail.com>
2014-01-03 00:19:41 +0100

    [fix] pep8: remove whitespaces

--- ics/eventlist.py
+++ ics/eventlist.py
@@ -29,7 +29,6 @@ class EventList(list):
 
     def __getitem__(self, sl):
         """Slices EventList.
-        
         |  If sl is conventional (like [10], [4:12], [3:100:2], [::-1], …),
             it slices the EventList like a classical list().
         |  If one of the 3 arguments ([start:stop:step]) is not None or an int,
