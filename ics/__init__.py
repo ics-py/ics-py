641843b8e4564d8c123a924afa6adf5a3bd2d35c
Daniel Téllez <danieltellez@flossfriday.com>
2018-02-12 13:01:27 +0100

    Missing declaration on __init__

--- ics/__init__.py
+++ ics/__init__.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals, absolute_import
 from .icalendar import Calendar
 from .event import Event
 from .alarm import AudioAlarm, DisplayAlarm
+from .attendee import Attendee
 from .__meta__ import (
     __title__,
     __version__,
