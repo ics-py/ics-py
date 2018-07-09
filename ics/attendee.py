412474b0625f1dd01873ca20cd57d598d6f8a0de
t00n <kelkununtel@hotmail.com>
2018-07-09 13:37:01 +0200

    [add] Attendee properties + tests

--- ics/attendee.py
+++ ics/attendee.py
@@ -8,8 +8,51 @@ class Attendee(object):
 
     def __init__(self, email, common_name=None, rsvp=None):
         self.email = email
-        self.common_name = common_name or email
+        self.common_name = common_name
         self.rsvp = rsvp
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if isinstance(value, str):
+            self._email = value
+        else:
+            raise ValueError("email should be a str")
+
+    @property
+    def common_name(self):
+        return self._common_name
+
+    @common_name.setter
+    def common_name(self, value):
+        if value is None:
+            self._common_name = self.email
+        elif isinstance(value, str):
+            self._common_name = value
+        else:
+            raise ValueError("common_name should be None or str")
+
+    @property
+    def rsvp(self):
+        return self._rsvp
+
+    @rsvp.setter
+    def rsvp(self, value):
+        if value is None:
+            self._rsvp = value
+        elif value is True:
+            self._rsvp = True
+        elif value is False:
+            self._rsvp = False
+        elif value.upper() == "TRUE":
+            self._rsvp = True
+        elif value.upper() == "FALSE":
+            self._rsvp = False
+        else:
+            raise ValueError("RSVP should be None, TRUE or FALSE")
 
     def __str__(self):
         """Returns the attendee in an iCalendar format."""
@@ -20,7 +63,7 @@ class Attendee(object):
         if self.common_name:
             params.update({'CN': ["'%s'" % self.common_name]})
 
-        if self.rsvp:
-            params.update({'RSVP': [self.rsvp]})
+        if self.rsvp is not None:
+            params.update({'RSVP': ["TRUE" if self.rsvp else "FALSE"]})
 
         return params
--- tests/event.py
+++ tests/event.py
@@ -155,6 +155,27 @@ class TestEvent(unittest.TestCase):
         line = str(a2)
         self.assertIn("ATTENDEE;CN='Email':mailto:email@email.com", line)
 
+        with self.assertRaises(ValueError):
+            Attendee(email=None)
+
+        with self.assertRaises(ValueError):
+            Attendee(email="test@test.com", common_name=5)
+
+        with self.assertRaises(ValueError):
+            Attendee(email="test@test.com", rsvp="lol")
+
+        b = Attendee(email="test@test.com", rsvp="TRUE")
+        self.assertTrue(b.rsvp)
+
+        b.rsvp = True
+        self.assertTrue(b.rsvp)
+
+        b.rsvp = "FALSE"
+        self.assertFalse(b.rsvp)
+
+        b.rsvp = False
+        self.assertFalse(b.rsvp)
+
     def test_add_attendees(self):
         e = Event()
         a = Attendee(email='email@email.com')
@@ -258,7 +279,7 @@ class TestEvent(unittest.TestCase):
     def test_category_output(self):
         cat = "Simple category"
         e = Event(name="Name", categories={cat})
-        self.assertIn("CATEGORIES:"+cat, str(e).splitlines())
+        self.assertIn("CATEGORIES:" + cat, str(e).splitlines())
 
     def test_all_day_with_end(self):
         c = Calendar(cal20)
@@ -419,3 +440,13 @@ class TestEvent(unittest.TestCase):
         event = Event(uid='0', name='Test #1', begin=dt(2016, 6, 10, 20, 10), duration=td(minutes=30))
         event.join(event)
         assert event == Event(uid='0', name='Test #1', begin=dt(2016, 6, 10, 20, 10), duration=td(minutes=30))
+
+    def test_event_attendee(self):
+        attendee_a = Attendee(email="test@test.com", common_name="Test", rsvp="TRUE")
+        attendee_b = Attendee(email="test2@test.com")
+        event_a = Event(name="Test #1", attendees={attendee_a, attendee_b})
+
+        text = str(event_a)
+
+        self.assertIn("ATTENDEE;CN='Test';RSVP=TRUE:mailto:test@test.com", text)
+        self.assertIn("ATTENDEE;CN='test2@test.com':mailto:test2@test.com", text)
