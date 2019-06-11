b76c3943c73c7a5bfb2bd8d8b107eda304e59203
Nikita Marchant <nikita.marchant@gmail.com>
2019-06-11 23:20:18 +0200

    Drop support for python 3.4, add support for 3.7 (#179)

--- setup.py
+++ setup.py
@@ -56,9 +56,9 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
     ],
     url='http://github.com/C4ptainCrunch/ics.py',
