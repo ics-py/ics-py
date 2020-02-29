78985679e5eff4e6446eb9edad4816955d1a85a5
Nikita Marchant <nikita.marchant@gmail.com>
2020-02-29 19:18:19 +0100

    Bump version

--- ics/__meta__.py
+++ ics/__meta__.py
@@ -1,5 +1,5 @@
-__title__ = 'ics'
-__version__ = '0.7dev'
-__author__ = 'Nikita Marchant'
-__license__ = 'Apache License, Version 2.0'
-__copyright__ = 'Copyright 2013-2019 Nikita Marchant and individual contributors'
+__title__ = "ics"
+__version__ = "0.7"
+__author__ = "Nikita Marchant"
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2013-2020 Nikita Marchant and individual contributors"
