868ef001f7d793f8e380a1c59072cbd258b6936f
Simon Dominik Niko Fink <simon.fink@uni-passau.de>
2021-08-18 21:17:25 +0200

    fix alarm conversion

--- src/ics/alarm.py
+++ src/ics/alarm.py
@@ -1,15 +1,14 @@
-from abc import ABCMeta, abstractmethod
-from datetime import datetime, timedelta
-from typing import List, Union, Type
-
 import attr
+from abc import ABCMeta, abstractmethod
 from attr.converters import optional as c_optional
 from attr.validators import instance_of, optional as v_optional
+from datetime import datetime, timedelta
+from typing import List, Union, Type
 
 from ics.attendee import Attendee
 from ics.component import Component
 from ics.types import URL
-from ics.utils import call_validate_on_inst, check_is_instance, ensure_timedelta
+from ics.utils import check_is_instance, ensure_timedelta
 
 __all__ = ["BaseAlarm", "AudioAlarm", "CustomAlarm", "DisplayAlarm", "EmailAlarm", "NoneAlarm", "get_type_from_action"]
 
@@ -24,25 +23,9 @@ class BaseAlarm(Component, metaclass=ABCMeta):
     trigger: Union[timedelta, datetime, None] = attr.ib(
         default=None,
         validator=v_optional(instance_of((timedelta, datetime)))
-    )  # TODO is this relative to begin or end?
-    repeat: int = attr.ib(default=None, validator=call_validate_on_inst)
-    duration: timedelta = attr.ib(default=None, converter=c_optional(ensure_timedelta), validator=call_validate_on_inst)  # type: ignore[misc]
-
-    # FIXME: `attach` can be specified multiple times in a "VEVENT", "VTODO", "VJOURNAL", or "VALARM" calendar component
-    #  with the exception of AUDIO alarm that only allows this property to occur once.
-    #  (This property is used in "VALARM" calendar components to specify an audio sound resource or an email message attachment.)
-
-    def validate(self, attr=None, value=None):
-        if self.repeat is not None:
-            if self.repeat < 0:
-                raise ValueError("Repeat must be great than or equal to 0.")
-            if self.duration is None:
-                raise ValueError(
-                    "A definition of an alarm with a repeating trigger MUST include both the DURATION and REPEAT properties."
-                )
-
-        if self.duration is not None and self.duration.total_seconds() < 0:
-            raise ValueError("Alarm duration timespan must be positive.")
+    )
+    repeat: int = attr.ib(default=None)
+    duration: timedelta = attr.ib(default=None, converter=c_optional(ensure_timedelta))  # type: ignore[misc]
 
     @property
     @abstractmethod
@@ -83,7 +66,7 @@ class DisplayAlarm(BaseAlarm):
     A calendar event VALARM with DISPLAY option.
     """
 
-    display_text: str = attr.ib(default=None)
+    description: str = attr.ib(default=None)
 
     @property
     def action(self):
@@ -96,14 +79,15 @@ class EmailAlarm(BaseAlarm):
     A calendar event VALARM with Email option.
     """
 
-    subject: str = attr.ib(default=None)
-    body: str = attr.ib(default=None)
-    recipients: List[Attendee] = attr.ib(factory=list)
+    summary: str = attr.ib(default=None)  # message subject
+    description: str = attr.ib(default=None)  # message body
+    attendees: List[Attendee] = attr.ib(factory=list, metadata={"ics_name": "ATTENDEE"})  # message recipients
+    attach: List[Union[URL, bytes, None]] = attr.ib(factory=list)  # e-mail attachments
 
-    def add_recipient(self, recipient: Attendee):
-        """ Add an recipient to the recipients list """
-        check_is_instance("recipient", recipient, Attendee)
-        self.recipients.append(recipient)
+    def add_attendee(self, attendee: Attendee):
+        """ Add an attendee to the attendee list """
+        check_is_instance("attendee", attendee, Attendee)
+        self.attendees.append(attendee)
 
     @property
     def action(self):
