a9b8e1a26dd6f1a779e35ef49edacbeb7dc60141
Nikita Marchant <nikita.marchant@gmail.com>
2022-06-30 10:00:09 +0200

    Packaging work for the upcoming release

--- ics/__meta__.py
+++ ics/__meta__.py
@@ -1,5 +1,5 @@
 __title__ = "ics"
-__version__ = "0.7"
+__version__ = "0.7.1"
 __author__ = "Nikita Marchant"
 __license__ = "Apache License, Version 2.0"
 __copyright__ = "Copyright 2013-2020 Nikita Marchant and individual contributors"
