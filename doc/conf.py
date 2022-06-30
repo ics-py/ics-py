20cdd95dcb0e64f201a6e248b131d9e300ff74fc
Nikita Marchant <nikita.marchant@gmail.com>
2022-06-30 15:25:14 +0200

    Try an upgrade to sphinx 5

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,7 @@ from ics import __version__
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.3'
+needs_sphinx = '5.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
